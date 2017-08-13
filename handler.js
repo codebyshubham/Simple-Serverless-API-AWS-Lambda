b6bd424d9e67dda3eb541de5d7b8feb76df1df60
Shubham Patel <shubhampatel518@gmail.com>
2017-08-13 16:29:07 +0530

    get user input

--- handler.js
+++ handler.js
@@ -1,6 +1,26 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+  if (event.queryStringParameters && event.queryStringParameters.name){
+    return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'hello ' + event.queryStringParameters.name + ', nice to meet you!',
+        })
+    });
+  }
+
+  if (event.httpMethod === "POST" && event.body){
+    let json = JSON.parse(event.body);
+    return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hi, I have Received Json Object from you.',
+            object: json
+        })
+    });
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
